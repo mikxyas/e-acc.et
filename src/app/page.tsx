d50e3c7be146f85134146f4c1bd799d07390d3d0
Mikiyas <mymikiaa11@gmail.com>
2024-02-05 13:41:52 +0300

    removed serif font from header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main style={{ backgroundImage: "url('ea.jpg')" }} className="bg-cover bg-no-repeat bg-center flex min-h-screen flex-col items-center justify-between p-24">
       <div className="absolute inset-0 bg-black/60 backdrop-blur-lg"></div>
 
-      <div className="flex mt-auto self-end items-center text-6xl font-serif z-10">
+      <div className="flex mt-auto self-end items-center text-6xl z-10">
         <Image alt="ET to the Moon!" width={73} height={63} src='/ealogo.png' />
         Effective Accleration
       </div>
